137271b6d1b4df9f213453fa6266ea5623aa02ed
sajib689 <jibonraja11@gmail.com>
2024-03-27 12:06:13 +0600

    localstorage added

---
+++ src/Components/Utilitis/index.js
@@ -0,0 +1,21 @@
+import toast from "react-hot-toast";
+export const getBooks = () => {
+    let books = [];
+    const storageBooks =localStorage.getItem('books')
+    if(storageBooks) {
+        books = JSON.parse(storageBooks)
+    }
+    return books
+}
+
+export const saveBooks = book => {
+    let books = getBooks()
+    const isExiting = books.find(b => b.bookId === book.bookId)
+    if (isExiting){
+        return toast.error("Already have!.")
+    }
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
+    toast.success("Book Bookmarked successfully")
+
+}
