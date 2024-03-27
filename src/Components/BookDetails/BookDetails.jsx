896259f499ee84cdc6bc08951a96a58ffdd6c4ed
sajib689 <jibonraja11@gmail.com>
2024-03-27 11:52:31 +0600

    add the read btn

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,35 +1,96 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
-
 const BookDetails = () => {
-    const books = useLoaderData()
-    const {bookId} = useParams()
-    const bookInt = parseInt(bookId)
-    const getbook = books.books
-    const book = getbook.find((b) => b.bookId === bookInt)
-    const {bookId,bookName,author,image,category,tags,rating} = book;
-    console.log(book)
-    return (
-        <section className="dark:bg-gray-100 dark:text-gray-800">
-	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-			<img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
-		</div>
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-				<span className="dark:text-violet-600">senectus</span>erat pharetra
-			</h1>
-			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-				<br  className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
-			</p>
-			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Suspendisse</a>
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800">Malesuada</a>
-			</div>
-		</div>
-	</div>
-</section>
-    );
+  const books = useLoaderData();
+  const { bookId } = useParams();
+  const bookInt = parseInt(bookId);
+  const getbook = books.books;
+  const book = getbook.find((b) => b.bookId === bookInt);
+  const {
+    bookName,
+    author,
+    image,
+    category,
+    tags,
+    rating,
+    review,
+    publisher,
+    yearOfPublishing,
+    totalPages,
+  } = book;
+  console.log(book);
+  return (
+    <section className="dark:bg-gray-100 dark:text-gray-800">
+      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+        <div className="flex bg-[#1313130D] rounded-lg md:w-[570px]  items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+          <img
+            src={image}
+            alt=""
+            className="p-5 object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+          />
+        </div>
+        <div className="flex flex-col  p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+            {bookName}
+          </h1>
+          <p className="mt-6 mb-8 text-lg sm:mb-12 text-[#131313CC]">
+            By : {author}
+          </p>
+          <hr />
+          <p>{category}</p>
+          <hr />
+          <p className="mt-2 mb-2">
+            <span>Review:</span> {review}
+          </p>
+          <div className="flex gap-4 mt-4 mb-4">
+            {tags.map((tag) => (
+              <button key={tag} className="btn btn-sm text-[#23BE0A]">
+                {tag}
+              </button>
+            ))}
+          </div>
+          <hr />
+          <table className="mt-5">
+            <tbody>
+              <tr>
+                <td>Number of Pages:</td>
+                <td>
+                  <span className="text-[#131313] font-bold">{totalPages}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>Publisher:</td>
+                <td>
+                  <span className="text-[#131313] font-bold">
+                    {yearOfPublishing}
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>Year of Publishing:</td>
+                <td>
+                  <span className="text-[#131313] font-bold">{publisher}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>Rating:</td>
+                <td>
+                  <span className="text-[#131313] font-bold">{rating}</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="mt-5 flex gap-3">
+        <button className="btn btn-active">Read</button>
+        <button className="btn btn-active btn-primary">Wishlist</button>
+
+
+        </div>
+        </div>
+      
+      </div>
+    </section>
+  );
 };
 
 export default BookDetails;
