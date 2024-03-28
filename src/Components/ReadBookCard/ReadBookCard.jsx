f20dd02eacdf54813ebea5fcec159a45bac24e6a
sajib689 <jibonraja11@gmail.com>
2024-03-28 12:04:50 +0600

    add success sorting

--- src/Components/ReadBookCard/ReadBookCard.jsx
+++ src/Components/ReadBookCard/ReadBookCard.jsx
@@ -40,16 +40,16 @@ const ReadBookCard = ({ book }) => {
           </div>
           <div className="mb-3 flex justify-start items-start gap-5">
             <div className="flex justify-center items-center gap-2">
-            <FaUserFriends /><p>Publisher: Scribner</p>
+            <FaUserFriends /><p>Publisher: {publisher}</p>
             </div>
             <div className="flex justify-center items-center gap-2">
-            <MdContactPage /><p>Page 192</p>
+            <MdContactPage /><p>Page {totalPages}</p>
             </div>
           </div>
           <hr />
           <div className="mt-3 flex justify-start items-start gap-5">
-            <button className="btn btn-sm text-[#328EFF]">Category: Classic</button>
-            <button className="btn btn-sm text-[#FFAC33]">Rating: 4.5</button>
+            <button className="btn btn-sm text-[#328EFF]">Category: {category}</button>
+            <button className="btn btn-sm text-[#FFAC33]">Rating: {rating}</button>
             <Link to={`bookdetails/${bookId}`} className="btn btn-sm bg-[#23BE0A] text-white">View Details</Link>
           </div>
         </div>
